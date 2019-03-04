f186369702051e5bf6c64ae12fc44cf4d41e3aca
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-03-03 20:23:17 -0800

    implement sticky-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,60 +15,63 @@ export class AppComponent implements OnInit {
     private _router: Subscription;
     @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
+    // tslint:disable-next-line: max-line-length
     constructor( private renderer : Renderer, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
     ngOnInit() {
         var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
-        this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
-            if (this.location.path() !== '/sections') {
-                if (window.outerWidth > 991) {
-                    window.document.children[0].scrollTop = 0;
-                }else{
-                    window.document.activeElement.scrollTop = 0;
-                }
-            }
-            this.navbar.sidebarClose();
+        // this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
+        //     // if (this.location.path() !== '/sections') {
+        //     //     if (window.outerWidth > 991) {
+        //     //         window.document.children[0].scrollTop = 0;
+        //     //     } else{
+        //     //         window.document.activeElement.scrollTop = 0;
+        //     //     }
+        //     // }
+        //     // this.navbar.sidebarClose();
 
-            this.renderer.listenGlobal('window', 'scroll', (event) => {
-                const number = window.scrollY;
-                var _location = this.location.path();
-                _location = _location.split('/')[2];
-                if (this.location.path() !== '/sections') {
+        //     // this.renderer.listenGlobal('window', 'scroll', (event) => {
+        //     //     const number = window.scrollY;
+        //     //     var _location = this.location.path();
+        //     //     _location = _location.split('/')[2];
+        //         // if (this.location.path() !== '/sections') {
 
-                    if (number > 150 || window.pageYOffset > 150) {
-                        // add logic
-                          if (_location !== 'register') {
-                                navbar.classList.remove('navbar-transparent');
-                          }
-                    } else if (_location !== 'addproduct'  && _location !== 'login' && _location !== 'register' && this.location.path() !== '/nucleoicons') {
-                        // remove logic
-                        navbar.classList.add('navbar-transparent');
-                    }
-                }
-            });
-        });
+        //             // if (number > 150 || window.pageYOffset > 150) {
+        //                 // add logic
+        //                 //   if (_location !== 'register') {
+        //                 //         navbar.classList.remove('navbar-transparent');
+        //                 //   }
+        // tslint:disable-next-line: max-line-length
+        //             // } else if (_location !== 'addproduct'  && _location !== 'login' && _location !== 'register' && this.location.path() !== '/nucleoicons') {
+        //             //     // remove logic
+        //             //     navbar.classList.add('navbar-transparent');
+        //             // }
+        //         // }
+        //     // });
+        // });
 
-        var ua = window.navigator.userAgent;
-        var trident = ua.indexOf('Trident/');
-        if (trident > 0) {
-            // IE 11 => return version number
-            var rv = ua.indexOf('rv:');
-            var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-        }
-        if (version) {
-            var body = document.getElementsByTagName('body')[0];
-            body.classList.add('ie-background');
+        // var ua = window.navigator.userAgent;
+        // var trident = ua.indexOf('Trident/');
+        // if (trident > 0) {
+        //     // IE 11 => return version number
+        //     var rv = ua.indexOf('rv:');
+        //     var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        // }
+        // if (version) {
+        //     var body = document.getElementsByTagName('body')[0];
+        //     body.classList.add('ie-background');
 
-        }
+        // }
 
     }
-    removeFooter() {
-        var titlee = this.location.prepareExternalUrl(this.location.path());
-        titlee = titlee.slice( 1 );
-        if(titlee === 'signup' || titlee === 'nucleoicons'){
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
+
+    // removeFooter() {
+    //     var titlee = this.location.prepareExternalUrl(this.location.path());
+    //     titlee = titlee.slice( 1 );
+    //     if(titlee === 'signup' || titlee === 'nucleoicons'){
+    //         return false;
+    //     }
+    //     else {
+    //         return true;
+    //     }
+    // }
 }
