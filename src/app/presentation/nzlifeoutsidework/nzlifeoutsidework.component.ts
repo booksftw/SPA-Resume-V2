5a10bceb78f6038974c7ae6cd7179e36404ea806
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-03-04 20:51:46 -0800

    move blocks around

---
+++ src/app/presentation/nzlifeoutsidework/nzlifeoutsidework.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nzlifeoutsidework',
+  templateUrl: './nzlifeoutsidework.component.html',
+  styleUrls: ['./nzlifeoutsidework.component.scss']
+})
+export class NzlifeoutsideworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
