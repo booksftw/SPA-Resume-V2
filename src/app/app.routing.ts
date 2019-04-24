fa5376ea40f8ba371f0adeacac6795436bfdc821
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-04-24 00:41:13 -0700

    update routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PresentationComponent } from './presentation/presentation.component';
 
-const routes: Routes =[
-    { path: '', redirectTo: 'resume', pathMatch: 'full' },
-    { path: 'resume',         component: PresentationComponent },
+const routes: Routes = [
+    { path: '', pathMatch: 'full', component: PresentationComponent },
+    // { path: '', redirectTo: 'resume', pathMatch: 'full' },
+    // { path: 'resume',         component: PresentationComponent },
 ];
 
 @NgModule({
